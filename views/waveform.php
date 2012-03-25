48363d2742fd870f32216b667543f51cd630061b
Matt Carter <m@ttcarter.com>
2012-03-25 12:50:41 +1000

    BUGFIX: Fixes to sparks style Waveform view

--- views/waveform.php
+++ views/waveform.php
@@ -1,16 +1,12 @@
-<?
+<?php
 /**
-Simple Waveform form output view
+* Simple Waveform form output view
 *
 * Optional named parameters:
 *
-* @param string $header Either HTML or plaintext header to output (if plaintext, will be wrapped in <p> tags)
+* @param string $header Either HTML or plaintext header to output (if plaintext, it will be wrapped in <p> tags)
+*
 */
-<? $this->load->view('jquery/itip') ?>
-<? if (isset($header)) { // Output header (if any)
-	if (substr($header, 0, 1) == '<') { // Assume header is already HTML and just paste
-		echo $header;
-	} else
-		echo "<p>$header</p>";
-} ?>
-<? $this->Waveform->Form() ?>
+if (isset($header)) // Output header (if any)
+	echo (substr($header, 0, 1) == '<') ? $header : "<p>$header</p>";
+$this->waveform->Form();
